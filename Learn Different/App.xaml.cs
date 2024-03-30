d138d0cba856c04259b617ef2268092eb82fc9ae
kraftbre@iastate.edu <glpat-ViGhVtPBZraraX9xnK9a>
2024-03-30 11:32:58 -0400

    Set Up Project

---
+++ Learn Different/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Learn_Different;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Learn Different/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Learn_Different;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
